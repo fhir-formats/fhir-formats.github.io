eb4ae2130d9f99d6a312b152132bd73181ba7c87
Vadim Peretokin <vperetokin@gmail.com>
2016-04-22 20:03:20 +1000

    Added support for clearing views

--- main.js
+++ main.js
@@ -32,6 +32,11 @@ function FhirInputView(el) {
       }
     oldValue = text;
 
+    if (text === "") {
+      clearOtherView();
+      return;
+    }
+
     /* update code mirror's highlighting mode */
     codemirror.setOption("mode", looksLikeJson(text) ? "javascript" : "xml");
     /* convert data to other format and send to opposite codemirror */
@@ -63,6 +68,11 @@ function FhirInputView(el) {
     // }
   }
 
+  function clearOtherView() {
+    var otherView = leftFhirView.codemirror === codemirror ? rightFhirView : leftFhirView;
+    otherView.setText("");
+  }
+
   var self = this;
 }
 
@@ -80,12 +90,14 @@ var leftFhirView = new FhirInputView(document.getElementById('fhir-input-left'))
 var rightFhirView = new FhirInputView(document.getElementById('fhir-input-right'));
 
 document.getElementById('skeleton-patient__link').onclick = function () {
-  leftFhirView.setText("<Patient xmlns='http://hl7.org/fhir'>\n  <identifier>\n    <type>\n      <coding>\n        <system value='http://hl7.org/fhir/v2/0203'/>\n        <code value='MR'/>\n      </coding>\n    </type>\n    <use value='usual'/>\n  </identifier>\n  <id value='example'/>\n</Patient>");
+  var xml = runLuaSafe("in_fhir_xml[[{\"identifier\":[{\"type\":{\"coding\":[{\"code\":\"MR\",\"system\":\"http://hl7.org/fhir/v2/0203\"}]},\"use\":\"usual\"}],\"resourceType\":\"Patient\",\"id\":\"example\"}]]").join();
+  leftFhirView.setText(xml);
   document.querySelector('.mdl-layout').MaterialLayout.toggleDrawer();
 }
 
   document.getElementById('full-patient__link').onclick = function () {
-  leftFhirView.setText("<Patient xmlns='http://hl7.org/fhir'>\n  <active value='true'/>\n  <gender value='male'/>\n  <address>\n    <period>\n      <start value='1974-12-25'/>\n    </period>\n    <line value='534 Erewhon St'/>\n    <district value='Rainbow'/>\n    <use value='home'/>\n    <state value='Vic'/>\n    <city value='PleasantVille'/>\n    <postalCode value='3999'/>\n    <type value='both'/>\n  </address>\n  <contact>\n    <name>\n      <given value='Bénédicte'/>\n      <family value='du'>\n        <extension url='http://hl7.org/fhir/StructureDefinition/iso21090-EN-qualifier'>\n          <valueCode value='VV'/>\n        </extension>\n      </family>\n      <family value='Marché'/>\n    </name>\n    <gender value='female'/>\n    <period>\n      <start value='2012'/>\n    </period>\n    <relationship>\n      <coding>\n        <code value='partner'/>\n        <system value='http://hl7.org/fhir/patient-contact-relationship'/>\n      </coding>\n    </relationship>\n    <telecom>\n      <value value='+33 (237) 998327'/>\n      <system value='phone'/>\n    </telecom>\n  </contact>\n  <telecom>\n    <use value='home'/>\n  </telecom>\n  <telecom>\n    <system value='phone'/>\n    <use value='work'/>\n    <value value='(03) 5555 6473'/>\n  </telecom>\n  <name>\n    <family value='Chalmers'/>\n    <given value='Peter'/>\n    <given value='James'/>\n    <use value='official'/>\n  </name>\n  <name>\n    <given value='Jim'/>\n    <use value='usual'/>\n  </name>\n  <birthDate value='1974-12-25'>\n    <extension url='http://hl7.org/fhir/StructureDefinition/patient-birthTime'>\n      <valueDateTime value='1974-12-25T14:35:45-05:00'/>\n    </extension>\n  </birthDate>\n  <managingOrganization>\n    <reference value='Organization/1'/>\n  </managingOrganization>\n  <deceasedBoolean value='false'/>\n  <identifier>\n    <type>\n      <coding>\n        <code value='MR'/>\n        <system value='http://hl7.org/fhir/v2/0203'/>\n      </coding>\n    </type>\n    <use value='usual'/>\n    <assigner>\n      <display value='Acme Healthcare'/>\n    </assigner>\n    <system value='urn:oid:1.2.36.146.595.217.0.1'/>\n    <period>\n      <start value='2001-05-06'/>\n    </period>\n    <value value='12345'/>\n  </identifier>\n  <id value='example'/>\n  <text>\n    <div xmlns='http://www.w3.org/1999/xhtml'>\n      <table>\n        <tbody>\n          <tr>\n            <td>Name</td>\n            <td>Peter James<b>Chalmers</b>(\"Jim\")</td>\n          </tr>\n          <tr>\n            <td>Address</td>\n            <td>534 Erewhon, Pleasantville, Vic, 3999</td>\n          </tr>\n          <tr>\n            <td>Contacts</td>\n            <td>Home: unknown. Work: (03) 5555 6473</td>\n          </tr>\n          <tr>\n            <td>Id</td>\n            <td>MRN: 12345 (Acme Healthcare)</td>\n          </tr>\n        </tbody>\n      </table>\n    </div>\n    <status value='generated'/>\n  </text>\n</Patient>");
+    var xml = runLuaSafe("in_fhir_xml[[{\"_birthDate\":{\"extension\":[{\"valueDateTime\":\"1974-12-25T14:35:45-05:00\",\"url\":\"http://hl7.org/fhir/StructureDefinition/patient-birthTime\"}]},\"contact\":[{\"relationship\":[{\"coding\":[{\"code\":\"partner\",\"system\":\"http://hl7.org/fhir/patient-contact-relationship\"}]}],\"gender\":\"female\",\"period\":{\"start\":\"2012\"},\"name\":{\"family\":[\"du\",\"Marché\"],\"_family\":[{\"extension\":[{\"valueCode\":\"VV\",\"url\":\"http://hl7.org/fhir/StructureDefinition/iso21090-EN-qualifier\"}]},null],\"given\":[\"Bénédicte\"]},\"telecom\":[{\"system\":\"phone\",\"value\":\"+33 (237) 998327\"}]}],\"id\":\"example\",\"address\":[{\"use\":\"home\",\"postalCode\":\"3999\",\"city\":\"PleasantVille\",\"line\":[\"534 Erewhon St\"],\"period\":{\"start\":\"1974-12-25\"},\"type\":\"both\",\"district\":\"Rainbow\",\"state\":\"Vic\"}],\"telecom\":[{\"use\":\"home\"},{\"use\":\"work\",\"system\":\"phone\",\"value\":\"(03) 5555 6473\"}],\"resourceType\":\"Patient\",\"active\":true,\"birthDate\":\"1974-12-25\",\"deceasedBoolean\":false,\"text\":{\"status\":\"generated\",\"div\":\"<div xmlns='http://www.w3.org/1999/xhtml'>\\n  <table>\\n    <tbody>\\n      <tr>\\n        <td>Name</td>\\n        <td>Peter James<b>Chalmers</b>(\\\"Jim\\\")</td>\\n      </tr>\\n      <tr>\\n        <td>Address</td>\\n        <td>534 Erewhon, Pleasantville, Vic, 3999</td>\\n      </tr>\\n      <tr>\\n        <td>Contacts</td>\\n        <td>Home: unknown. Work: (03) 5555 6473</td>\\n      </tr>\\n      <tr>\\n        <td>Id</td>\\n        <td>MRN: 12345 (Acme Healthcare)</td>\\n      </tr>\\n    </tbody>\\n  </table>\\n</div>\"},\"gender\":\"male\",\"identifier\":[{\"use\":\"usual\",\"system\":\"urn:oid:1.2.36.146.595.217.0.1\",\"period\":{\"start\":\"2001-05-06\"},\"type\":{\"coding\":[{\"code\":\"MR\",\"system\":\"http://hl7.org/fhir/v2/0203\"}]},\"assigner\":{\"display\":\"Acme Healthcare\"},\"value\":\"12345\"}],\"name\":[{\"use\":\"official\",\"given\":[\"Peter\",\"James\"],\"family\":[\"Chalmers\"]},{\"given\":[\"Jim\"],\"use\":\"usual\"}],\"managingOrganization\":{\"reference\":\"Organization/1\"}}]]").join();
+    leftFhirView.setText(xml);
   document.querySelector('.mdl-layout').MaterialLayout.toggleDrawer();
 }
 
